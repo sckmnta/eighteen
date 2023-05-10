ae023b0f0a7079c5a731bfb1fd615ad37dad44f8
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-05-10 13:51:35 +0300

    selenoid test added

--- src/test/java/tests/SelenoidTest.java
+++ src/test/java/tests/SelenoidTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class SelenoidTest {
@@ -21,5 +22,14 @@ public class SelenoidTest {
                 .then()
                 .body("total", is(20));
 
+    }
+
+    @Test
+    void checkTotalWithGiven(){
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
     }
 }
