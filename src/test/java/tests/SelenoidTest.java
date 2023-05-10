e0fa3f1e92e78ea3c9c9b627bf49607a413c27be
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-05-11 00:19:50 +0300

    -

--- src/test/java/tests/SelenoidTest.java
+++ src/test/java/tests/SelenoidTest.java
@@ -3,7 +3,10 @@ package tests;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SelenoidTest {
 
@@ -16,7 +19,7 @@ public class SelenoidTest {
      */
 
     @Test
-    void checkTotal(){
+    void checkTotal() {
 
         get("https://selenoid.autotests.cloud/status")
                 .then()
@@ -25,11 +28,104 @@ public class SelenoidTest {
     }
 
     @Test
-    void checkTotalWithGiven(){
+    void checkTotalWithGiven() {
         given()
                 .when()
                 .get("https://selenoid.autotests.cloud/status")
                 .then()
                 .body("total", is(20));
     }
+
+    @Test
+    void checkTotalWithStatus() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkWIthAllLogs() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkWIthSomeLogs() {
+        given()
+                .log().uri()
+                //.log().body() for post() requests
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkChromeVersion() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+    @Test
+    void checkResponseBadPractice() { // BAD PRACTICE111
+        String expectedResponse = "{total:20,used:0,queued:0,pending:0,browsers:{"+
+                "android:{8.1:{}},chrome:{100.0:{},99.0:{}},"+
+                "chrome-mobile:{86.0:{}},firefox:{97.0:{},98.0:{}},opera:{84.0:{},85.0:{}}}}"; //spaces mistake, does not matter
+        String actualResponse = given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().response().asString();
+        assertEquals(expectedResponse, actualResponse);
+    }
+    @Test
+    void checkResponseWithGoodPractice(){
+        Integer expectedTotal = 20;
+       Integer actualTotal = given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().path("total");
+        assertEquals(expectedTotal, actualTotal);
+    }
+    @Test
+    void checkJSONScheme(){
+        given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("100.0"))
+                .body(matchesJsonSchemaInClasspath("schemes/status-scheme-response.json"));
+    }
 }
+
---
+++ src/test/java/tests/reqresInTests.java
@@ -0,0 +1,95 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class reqresInTests {
+
+     /*
+    1. Make request (POST) to https://reqres.in/api/login with body {"email": "eve.holt@reqres.in","password": "cityslicka"}
+    2. Get response {"token": "QpwL5tke4Pnpja7X4"}
+    3. Check if token equals QpwL5tke4Pnpja7X4
+     */
+
+
+
+    @Test
+    void reqresLoginTest(){
+
+        String body = "{\"email\": \"eve.holt@reqres.in\",\"password\": \"cityslicka\"}";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void reqresUnsuccesfulLoginTestWithoutMail(){
+
+        String body = "{\"password\": \"cityslicka\"}";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void reqresUnsuccesfulLoginTestWithoutPassword(){
+
+        String body = "{\"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void reqresUnsuccesfulLoginTestWithEmptyData(){
+
+        String body = "{ }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+
+}
